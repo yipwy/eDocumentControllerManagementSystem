9903beef9dc5c6fb71ba1949a27653aa125fd8c8
yipwy <yip_wy@huayang.com.my>
2019-07-07 22:18:03 +0800

    delta update

--- Source/eDCMS/generals/models.py
+++ Source/eDCMS/generals/models.py
@@ -8,7 +8,7 @@ class DocumentType(models.Model):
         verbose_name_plural     = 'Document_Types'
     document_code               = models.CharField(max_length=5)
     document_description        = models.CharField(max_length=50)
-    document_number_seriesId    = models.CharField(max_length=30)
+    document_number_seriesId    = models.ForeignKey('SeriesNumber', null=True, blank=True, on_delete=models.CASCADE)
     is_active                   = models.BooleanField(default=False)
     created_by                  = models.CharField(max_length=20)
     created_date                = models.DateTimeField(default=datetime.now(), blank=True)
@@ -17,4 +17,20 @@ class DocumentType(models.Model):
 
     def __str__(self):
         return self.document_description
-# Create your models here.
+
+class SeriesNumber(models.Model):
+    class Meta:
+        verbose_name_plural     = 'Series_Numbers'
+    series_code                 = models.CharField(max_length=5)
+    series_description          = models.CharField(max_length=50)
+    starting_number             = models.IntegerField()
+    ending_number               = models.IntegerField()
+    next_number                 = models.IntegerField()
+    is_active                   = models.BooleanField(default=False)
+    created_by                  = models.CharField(max_length=20)
+    created_date                = models.DateTimeField(default=datetime.now(), blank=True)
+    modify_by                   = models.CharField(max_length=20)
+    modify_date                 = models.DateTimeField(default=datetime.now(), blank=True)
+
+    def __str__(self):
+        return self.document_description
