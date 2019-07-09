f38a44a24a95af3fb52615cdbca9d7bbe3293e21
yipwy <yip_wy@huayang.com.my>
2019-07-09 16:22:31 +0800

    delta update

--- Source/eDCMS/generals/models.py
+++ Source/eDCMS/generals/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from datetime import datetime
 
 
-# Create your models here.
 class DocumentType(models.Model):
     class Meta:
-        verbose_name_plural     = 'Document_Types'
-    document_code               = models.CharField(max_length=5)
-    document_description        = models.CharField(max_length=50)
-    document_number_seriesId    = models.ForeignKey('SeriesNumber', null=True, blank=True, on_delete=models.CASCADE)
-    is_active                   = models.BooleanField(default=False)
-    created_by                  = models.CharField(max_length=20)
-    created_date                = models.DateTimeField(default=datetime.now(), blank=True)
-    modify_by                   = models.CharField(max_length=20)
-    modify_date                 = models.DateTimeField(default=datetime.now(), blank=True)
+        verbose_name_plural = 'DocumentTypes'
+
+    document_code = models.CharField(max_length=5)
+    document_description = models.CharField(max_length=50)
+    document_number_seriesId = models.ForeignKey('SeriesNumber', null=True, blank=True, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+    created_by = models.CharField(max_length=20)
+    created_date = models.DateTimeField(default=datetime.now(), blank=True)
+    modify_by = models.CharField(max_length=20)
+    modify_date = models.DateTimeField(default=datetime.now(), blank=True)
 
     def __str__(self):
         return self.document_description
@@ -21,17 +21,18 @@ class DocumentType(models.Model):
 
 class SeriesNumber(models.Model):
     class Meta:
-        verbose_name_plural     = 'Series_Numbers'
-    series_code                 = models.CharField(max_length=5)
-    series_description          = models.CharField(max_length=50)
-    starting_number             = models.IntegerField()
-    ending_number               = models.IntegerField()
-    next_number                 = models.IntegerField()
-    is_active                   = models.BooleanField(default=False)
-    created_by                  = models.CharField(max_length=20)
-    created_date                = models.DateTimeField(default=datetime.now(), blank=True)
-    modify_by                   = models.CharField(max_length=20)
-    modify_date                 = models.DateTimeField(default=datetime.now(), blank=True)
+        verbose_name_plural = 'Series_Numbers'
+
+    series_code = models.CharField(max_length=5)
+    series_description = models.CharField(max_length=50)
+    starting_number = models.IntegerField()
+    ending_number = models.IntegerField()
+    next_number = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+    created_by = models.CharField(max_length=20)
+    created_date = models.DateTimeField(default=datetime.now(), blank=True)
+    modify_by = models.CharField(max_length=20)
+    modify_date = models.DateTimeField(default=datetime.now(), blank=True)
 
     def __str__(self):
-        return self.document_description
+        return self.series_description
