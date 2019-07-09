184466b54216a55aa49a2a0642260fdd17d2a40f
sam_hs <samhuishen99@gmail.com>
2019-07-09 16:38:09 +0800

    added branch, department, warehouse tables

--- Source/eDCMS/accounts/models.py
+++ Source/eDCMS/accounts/models.py
@@ -10,9 +10,9 @@ class Profile(models.Model):
     departmentId     = models.CharField(max_length=50)
     is_active        = models.BooleanField(default=False)
     created_by       = models.CharField(max_length=20)
-    created_date     = models.DateTimeField(default=datetime.now(), blank=True)
+    created_date     = models.DateTimeField(default=datetime.now, blank=True)
     modify_by        = models.CharField(max_length=20)
-    modify_date      = models.DateTimeField(default=datetime.now(), blank=True)
+    modify_date      = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.companyId
