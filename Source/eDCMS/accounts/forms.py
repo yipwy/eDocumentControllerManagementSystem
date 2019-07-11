420f2d352679739b4f7645a58581888c5810c7d9
samhuishen99 <samhuishen99@gmail.com>
2019-07-11 23:33:34 +0800

    added sign up page

--- Source/eDCMS/accounts/forms.py
+++ Source/eDCMS/accounts/forms.py
@@ -4,10 +4,17 @@ from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
-
+    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Password Confirmation", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    departmentId = forms.CharField(label="Department", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    companyId = forms.CharField(label="Company", widget=forms.TextInput(attrs={'class': 'form-control'}))
     class Meta(UserCreationForm):
         model = Profile
-        fields = ('username', 'email', 'password1', 'password2', 'departmentId', 'companyId')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'departmentId', 'companyId')
 
 
 # class CustomUserChangeForm(UserChangeForm):
