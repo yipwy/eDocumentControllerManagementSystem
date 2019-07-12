91187baa20f11f7395a3d9be1159089e5e5bcff4
sam_hs <samhuishen99@gmail.com>
2019-07-12 10:55:27 +0800

    design sign up page

--- Source/eDCMS/accounts/forms.py
+++ Source/eDCMS/accounts/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Profile
 
-
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs={'class': 'form-row col form-group'}))
+    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs={'class': 'form-row col form-group'}))
     username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label="Password Confirmation", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     departmentId = forms.CharField(label="Department", widget=forms.TextInput(attrs={'class': 'form-control'}))
     companyId = forms.CharField(label="Company", widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta(UserCreationForm):
         model = Profile
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'departmentId', 'companyId')
