b785ca513c9b54a84a281e83d6fa2f99d10d35f8
Chee19 <chee.h19@gmail.com>
2019-07-22 11:42:32 +0800

    Merge issue fixed, add function implemented. Display of new data

---
+++ Source/eDCMS/recordmgnts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Container
+from generals.models import Warehouse
+
+
+class ContainerForm(forms.ModelForm):
+    warehouse = forms.ModelChoiceField(queryset=Warehouse.objects.all(), empty_label=None),
+    container_serial_number = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter serial number'})),
+    container_description = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Enter description'}), max_length=50
+    )
+
+    class Meta:
+        model = Container
+        fields = ['container_serial_number', 'container_description', 'warehouse']
