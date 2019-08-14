6fe59d33cb6e8c91b7201fd91cbbfb406537acfc
Chee19 <chee.h19@gmail.com>
2019-08-14 12:22:53 +0800

    replaced tinymce with froala editor. workable interface

--- Source/eDCMS/recordmgnts/admin.py
+++ Source/eDCMS/recordmgnts/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from .models import Container
 from simple_history.admin import SimpleHistoryAdmin
 from django.contrib.admin.models import LogEntry
+# from django_summernote.admin import SummernoteModelAdmin
 
 
 class ContainerAdmin(SimpleHistoryAdmin):
     list_display = ('container_serial_number', 'container_description', 'status', 'created_by', 'department',
                     'created_date', 'modify_by', 'modify_date', 'warehouse', 'bay', 'row', 'column')
     history_list_display = ["status"]
+    # summernote_fields = ['container_description']
 
 
 admin.site.register(Container, ContainerAdmin)
